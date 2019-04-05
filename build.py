8b133f751fead1f536056786ac85244f528c1a81
nkshigeru <1712694+nkshigeru@users.noreply.github.com>
2019-04-06 01:32:45 +0900

    Separate shared and static

--- build.py
+++ build.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder(pure_c=True)
+    builder = build_template_default.get_builder(pure_c=False)
+    build_shared = os.getenv("BUILD_SHARED", "True") == "True"
+    builder.remove_build_if(lambda build: build.options["librealsense:shared"] != build_shared)
     builder.run()
